14209c40812fbeeed3d807dcb964a5f0c092647a
eelakiya305-web <eelakiya305@gmail.com>
2026-07-25 10:59:14 +0530

    pattern2

---
+++ pattern2.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pattern(self,n):
+        for i in range(n):
+            for j in range(i+1):
+                print("*",end="")
+            print()
+ob=Solution()
+ob.pattern(5)
