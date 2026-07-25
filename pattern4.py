0aaf935e85ac6ef8d6f14f4c4902d5d4eca2cafc
eelakiya305-web <eelakiya305@gmail.com>
2026-07-25 11:18:26 +0530

    Pattern4

---
+++ pattern4.py
@@ -0,0 +1,8 @@
+class Solution:
+    def pattern(self,n):
+        for i in range(1,n+1):
+            for j in range(i):
+                print(i,end="")
+            print()
+ob=Solution()
+ob.pattern(5)
